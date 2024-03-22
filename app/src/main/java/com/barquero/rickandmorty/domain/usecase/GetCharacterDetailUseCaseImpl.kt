fdca080896ec17fb0c4651204175fc8e2f463607
Daniel Barquero <abarquero3@gmail.com>
2024-03-22 13:22:57 -0500

    updating dispatchers on usecase

--- app/src/main/java/com/barquero/rickandmorty/domain/usecase/GetCharacterDetailUseCaseImpl.kt
+++ app/src/main/java/com/barquero/rickandmorty/domain/usecase/GetCharacterDetailUseCaseImpl.kt
@@ -2,11 +2,15 @@ package com.barquero.rickandmorty.domain.usecase
 
 import com.barquero.rickandmorty.data.model.CharacterInfoApiModel
 import com.barquero.rickandmorty.data.repository.CharacterDetailRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class GetCharacterDetailUseCaseImpl(
     val repository: CharacterDetailRepository
 ) : GetCharacterDetailUseCase {
 
     override suspend fun execute(characterId: Int): Result<CharacterInfoApiModel> =
-        repository.requestCharacter(characterId)
+        withContext(Dispatchers.IO) {
+            repository.requestCharacter(characterId)
+        }
 }
--- app/src/main/java/com/barquero/rickandmorty/domain/usecase/GetCharacterUseCaseImpl.kt
+++ app/src/main/java/com/barquero/rickandmorty/domain/usecase/GetCharacterUseCaseImpl.kt
@@ -2,8 +2,10 @@ package com.barquero.rickandmorty.domain.usecase
 
 import com.barquero.rickandmorty.data.model.CharacterApiModel
 import com.barquero.rickandmorty.data.repository.CharacterRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
 
 class GetCharactersUseCaseImpl(private val characterRepository: CharacterRepository) :
     GetCharactersUseCase {
@@ -11,6 +13,6 @@ class GetCharactersUseCaseImpl(private val characterRepository: CharacterReposit
         return characterRepository.requestCharacters()
             .catch {
                 emit(Result.failure(it))
-            }
+            }.flowOn(Dispatchers.IO)
     }
 }
